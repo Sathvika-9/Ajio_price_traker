1d230ffe2976ed0e9324092ced587ca576da6e88
juttukondasriram <103946839+juttukondasriram@users.noreply.github.com>
2023-05-04 16:04:20 +0530

    Update test_get_ajio_product_info.py

--- test/test_get_ajio_product_info.py
+++ test/test_get_ajio_product_info.py
@@ -1,17 +1,21 @@
-
 import pytest
 from Ajio_price_traker.Ajio_price_traker import get_ajio_product_info
-
 
 @pytest.mark.parametrize("url", [
     "https://www.ajio.com/performax-zip-front-jacket-with-contrast-piping/p/443004374_white",
     "https://www.ajio.com/performax-fastdry-active-training-jacket/p/441120188_jetblack",
-    "https://www.ajio.com/invalid-url",  # This URL is invalid and should raise an exception
-])
+    "https://www.ajio.com/invalid-url",  # This URL is invalid and should raise an exception])
 def test_get_ajio_product_info(url):
-    try:
+    """
+    Test that get_ajio_product_info returns the expected data and product ID
+    for valid and invalid URLs.
+    """
+    if "invalid-url" in url:
+        with pytest.raises(Exception):
+            data, product_id = get_ajio_product_info(url)
+    else:
         data, product_id = get_ajio_product_info(url)
-    except Exception as e:
-        pytest.fail(f"Exception raised for URL {url}: {str(e)}")
-    assert isinstance(data, dict)
-    assert isinstance(product_id, str)
+        assert isinstance(data, dict)
+        assert "name" in data
+        assert "price" in data
+        assert isinstance(product_id, str)
